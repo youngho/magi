a943e1a71c111f831ea7b72dd78910e47a79056f
cho007 <t4600@naver.com>
2017-03-14 14:55:10 +0900

    java compositeDUTMap 수정

--- src/main/java/kr/famos/bets/dutmap/singledut/service/SingleDutMapService.java
+++ src/main/java/kr/famos/bets/dutmap/singledut/service/SingleDutMapService.java
@@ -20,53 +20,55 @@ public class SingleDutMapService {
     SingleDutMapMapper singleDutMapMapper;
 
 
-    public String retrieveSingleDutMap(SingleDutMapDto singleDutMapDto) {
+
+    public String retrieveSingleDutMap(SingleDutMapDto singleDutMapDto){
 
         //NB_BIN과 같음
         List<SingleDutMapDto> lstSingleDutMapDto = singleDutMapMapper.retrieveSingleDutMap(singleDutMapDto);
-        Map<Integer, String> mapDutMap = new TreeMap<>();
+
         Integer maxBin = 0;
         for (SingleDutMapDto dut : lstSingleDutMapDto) {
             String strBin = dut.getDutMainBin().replaceAll(",", "");
+
             if (maxBin < strBin.length()) {
                 maxBin = strBin.length();
             }
         }
 
-        List<Map<String, SingleDutMapDto>> mapSingleDutMapDto = singleDutMapMapper.retrieveSingleDutMapMap(singleDutMapDto);
+        List<Map<String,SingleDutMapDto>> mapSingleDutMapDto = singleDutMapMapper.retrieveSingleDutMapMap(singleDutMapDto);
         List arraySingleDutMapDto = new ArrayList();
 
-        for (Map<String, SingleDutMapDto> map : mapSingleDutMapDto) {
-            Map<String, String> mapSingleDutMapDtoRow = new LinkedHashMap<String, String>();
-            for (Map.Entry<String, SingleDutMapDto> entry : map.entrySet()) {
+        for( Map<String,SingleDutMapDto> map :mapSingleDutMapDto){
+            Map<String,String> mapSingleDutMapDtoRow = new LinkedHashMap<String, String>();
+            for(Map.Entry<String, SingleDutMapDto>entry:map.entrySet()){
                 String key = entry.getKey();
                 String value = String.valueOf(entry.getValue());
-                if (key.equals("DUT_MAIN_BIN")) {
+                if(key.equals("DUT_MAIN_BIN")){
 
-                    String strBin = value.replaceAll(",", "");
-                    for (int i = 0; i < strBin.length(); i++) {
-                        mapSingleDutMapDtoRow.put("DUT" + Integer.toString(i + 1), Character.toString(strBin.charAt(i)));
+                    String strBin = value.replaceAll(",","");
+                    for(int i=0; i < strBin.length() ;i++){
+                        mapSingleDutMapDtoRow.put("MainBin"+Integer.toString(i+1),Character.toString(strBin.charAt(i)));
                     }
-                    for (int i = strBin.length(); maxBin > i; i++) {
-                        mapSingleDutMapDtoRow.put("DUT" + Integer.toString(i + 1), "0");
+                    for(int i=strBin.length(); maxBin > i; i++ ){
+                        mapSingleDutMapDtoRow.put("MainBin"+Integer.toString(i+1),"0");
                     }
 
-                } else {
-                    mapSingleDutMapDtoRow.put(key, value);
+                }else{
+                    mapSingleDutMapDtoRow.put(key,value);
                 }
 
             }
             arraySingleDutMapDto.add(mapSingleDutMapDtoRow);
-            mapSingleDutMapDtoRow = null;
+            mapSingleDutMapDtoRow=null;
         }
 
         Gson gson = new Gson();
 
-        String strJson = gson.toJson(arraySingleDutMapDto);
+        String  strJson = gson.toJson(arraySingleDutMapDto);
 //        String test = "[{" + "\"" + "Lotid" + "\"" + ":" + "\"" + "fdsafdsafdsa5" + "\"" + "}" + "," + "{" + "\"" + "Lotid" + "\"" + ":" + "\"" + "fdsafdsafdsa2" + "\"" + "}]";
-        if (arraySingleDutMapDto.size() == 0) {
-            strJson = "[{" + "\"" + "Message" + "\"" + ":" + "\"" + "no data" + "\"" + "}]";
-        }
+//        if (arraySingleDutMapDto.size() == 0) {
+//            strJson = "[{" + "\"" + "a" + "\"" + ":" + "\"" + "a" + "\"" + "," + "\"" + "3" + "\"" + ":" + "\"" + "c" + "\"" + "," + "\"" + "2" + "\"" + ":" + "\"" + "b" + "\"" + "}]";
+//        }
 
 
         return strJson;
--- src/main/java/kr/famos/bets/yieldAnalysis/ngbin/rest/NgBinController.java
+++ src/main/java/kr/famos/bets/yieldAnalysis/ngbin/rest/NgBinController.java
@@ -36,61 +36,10 @@ public class NgBinController {
     }
 
     @RequestMapping(value = "/retrieveNgBinJson", method = RequestMethod.POST)
-    public String retrieveSubBinJson(@RequestBody NgBinDto ngBinDto) {
+    public ResponseEntity<String> retrieveSubBinJson(@RequestBody NgBinDto ngBinDto) {
+        logger.debug("NgBinController - retireveNgBin 메소드");
 
-        //표현해야 할 NG_BIN리스트를 만들기 위해 NG_BIN_DTO를 LIST로가 져옴, MAP으로 가져오면 특정 필드를 꺼내오기 힘듬
-        List<NgBinDto> lstNgBinDto = ngBinMapper.retrieveNgBinList(ngBinDto);
-        //ngBin을 ","로 split해서 Map에 key 를 누적 put 해서 표현 해야 할 ngBin리스트를 만듬 값은 "" 없음
-        // 추후 NG_BIN_DTO 맴으로 가져와 여기서 만들어진 ngBin리스트를 추가 하고 맵으로 가져온 ngbin을 key, value로 put하면 없는 값은 "" 있는 값은 value로 치환될 것임
-        Map<Integer,String> mapNgBin = new TreeMap<>();
-        for(NgBinDto bin : lstNgBinDto){
-            String strBin = bin.getNgBin();
-            String[] arrayBin = strBin.split(",");
-            for(int i=0; i < arrayBin.length-2 ;i=i+2){
-                mapNgBin.put(Integer.parseInt(arrayBin[i]),"");
-            }
-        }
-
-
-
-        List<Map<String,NgBinDto>> mapNgBinDto = ngBinMapper.retrieveNgBinMap(ngBinDto);
-        List ArrayNgBinDto = new ArrayList();
-        int index;
-        for( Map<String,NgBinDto> map :mapNgBinDto){
-            Map<String,String> mapNgBinDtoRow = new LinkedHashMap<String, String>();
-            for(Map.Entry<String, NgBinDto>entry:map.entrySet()){
-                String key = entry.getKey();
-                String value = String.valueOf(entry.getValue());
-                if(key.equals("NG_BIN")){
-                    // 위에서 만든 ngBin리스트를 추가합
-                    for(Map.Entry<Integer, String>entry2:mapNgBin.entrySet()){
-                        String key2 = Integer.toString(entry2.getKey());
-                        mapNgBinDtoRow.put("ngBin"+key2,"");
-                    }
-                    //지금 읽어온 ngBin값을 put 해서 치환 함
-                    String[] strBin = value.split(",");
-                    for(int i=0; i < strBin.length-2 ;i=i+2){
-                        mapNgBinDtoRow.put("ngBin"+strBin[i],strBin[i+1]);
-                    }
-
-                }else{
-                    mapNgBinDtoRow.put(key,value);
-                }
-
-            }
-            ArrayNgBinDto.add(mapNgBinDtoRow);
-            mapNgBinDtoRow=null;
-        }
-
-        Gson gson = new Gson();
-
-        String  strJsonNgBinDto = gson.toJson(ArrayNgBinDto);
-//        String test = "[{" + "\"" + "Lotid" + "\"" + ":" + "\"" + "fdsafdsafdsa5" + "\"" + "}" + "," + "{" + "\"" + "Lotid" + "\"" + ":" + "\"" + "fdsafdsafdsa2" + "\"" + "}]";
-        if (ArrayNgBinDto.size() == 0){
-            strJsonNgBinDto =  "[{" + "\"" + "Message" + "\"" + ":" + "\"" + "no data" + "\"" + "}]";
-        }
-
-        return strJsonNgBinDto;
+        return new ResponseEntity<String>(ngBinService.retrieveNgBinMap(ngBinDto), HttpStatus.OK);
     }
 
 
