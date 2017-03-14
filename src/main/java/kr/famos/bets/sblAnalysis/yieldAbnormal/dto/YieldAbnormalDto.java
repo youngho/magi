3713e7e5f1317d8fbad72d4b36a1b7fe0e6b77ca
김영호 <xfile0304@gmail.com>
2017-03-14 18:04:51 +0900

    Yield Abnormal 일부 수정

--- src/main/java/kr/famos/bets/sblAnalysis/yieldAbnormal/dto/YieldAbnormalDto.java
+++ src/main/java/kr/famos/bets/sblAnalysis/yieldAbnormal/dto/YieldAbnormalDto.java
@@ -29,20 +29,47 @@ public class YieldAbnormalDto {
 
     private String endTimeStart = "";
     private String endTimeEnd = "";
-    private String input = "";
-    private String yield = "";
-    private String pass = "";
-    private String bin1 = "";
-    private String bin2 = "";
-    private String bin3 = "";
-    private String bin4 = "";
-    private String bin5 = "";
-    private String bin6 = "";
-    private String bin7 = "";
-    private String bin8 = "";
-    private String bin9 = "";
+
+    private String opInput = "";
+    private String opYield = "";
+    private String opPass = "";
+    private String opBin1 = "";
+    private String opBin2 = "";
+    private String opBin3 = "";
+    private String opBin4 = "";
+    private String opBin5 = "";
+    private String opBin6 = "";
+    private String opBin7 = "";
+    private String opBin8 = "";
+    private String opBin9 = "";
 
     private String sblMode = "";
+
+    private String hInput = "";
+    private String hYield = "";
+    private String hPass = "";
+    private String hBin1 = "";
+    private String hBin2 = "";
+    private String hBin3 = "";
+    private String hBin4 = "";
+    private String hBin5 = "";
+    private String hBin6 = "";
+    private String hBin7 = "";
+    private String hBin8 = "";
+    private String hBin9 = "";
+
+    private String tInput = "";
+    private String tYield = "";
+    private String tPass = "";
+    private String tBin1 = "";
+    private String tBin2 = "";
+    private String tBin3 = "";
+    private String tBin4 = "";
+    private String tBin5 = "";
+    private String tBin6 = "";
+    private String tBin7 = "";
+    private String tBin8 = "";
+    private String tBin9 = "";
 
     public String getPartNumber() {
         return partNumber;
@@ -114,6 +141,14 @@ public class YieldAbnormalDto {
 
     public void setHead(String head) {
         this.head = head;
+    }
+
+    public String getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(String boardId) {
+        this.boardId = boardId;
     }
 
     public String getSblResult() {
@@ -228,108 +263,100 @@ public class YieldAbnormalDto {
         this.endTimeEnd = endTimeEnd;
     }
 
-    public String getInput() {
-        return input;
+    public String getOpInput() {
+        return opInput;
     }
 
-    public void setInput(String input) {
-        this.input = input;
+    public void setOpInput(String opInput) {
+        this.opInput = opInput;
     }
 
-    public String getYield() {
-        return yield;
+    public String getOpYield() {
+        return opYield;
     }
 
-    public void setYield(String yield) {
-        this.yield = yield;
+    public void setOpYield(String opYield) {
+        this.opYield = opYield;
     }
 
-    public String getPass() {
-        return pass;
+    public String getOpPass() {
+        return opPass;
     }
 
-    public void setPass(String pass) {
-        this.pass = pass;
+    public void setOpPass(String opPass) {
+        this.opPass = opPass;
     }
 
-    public String getBin1() {
-        return bin1;
+    public String getOpBin1() {
+        return opBin1;
     }
 
-    public void setBin1(String bin1) {
-        this.bin1 = bin1;
+    public void setOpBin1(String opBin1) {
+        this.opBin1 = opBin1;
     }
 
-    public String getBin2() {
-        return bin2;
+    public String getOpBin2() {
+        return opBin2;
     }
 
-    public void setBin2(String bin2) {
-        this.bin2 = bin2;
+    public void setOpBin2(String opBin2) {
+        this.opBin2 = opBin2;
     }
 
-    public String getBin3() {
-        return bin3;
+    public String getOpBin3() {
+        return opBin3;
     }
 
-    public void setBin3(String bin3) {
-        this.bin3 = bin3;
+    public void setOpBin3(String opBin3) {
+        this.opBin3 = opBin3;
     }
 
-    public String getBin4() {
-        return bin4;
+    public String getOpBin4() {
+        return opBin4;
     }
 
-    public void setBin4(String bin4) {
-        this.bin4 = bin4;
+    public void setOpBin4(String opBin4) {
+        this.opBin4 = opBin4;
     }
 
-    public String getBin5() {
-        return bin5;
+    public String getOpBin5() {
+        return opBin5;
     }
 
-    public void setBin5(String bin5) {
-        this.bin5 = bin5;
+    public void setOpBin5(String opBin5) {
+        this.opBin5 = opBin5;
     }
 
-    public String getBin6() {
-        return bin6;
+    public String getOpBin6() {
+        return opBin6;
     }
 
-    public void setBin6(String bin6) {
-        this.bin6 = bin6;
+    public void setOpBin6(String opBin6) {
+        this.opBin6 = opBin6;
     }
 
-    public String getBin7() {
-        return bin7;
+    public String getOpBin7() {
+        return opBin7;
     }
 
-    public void setBin7(String bin7) {
-        this.bin7 = bin7;
+    public void setOpBin7(String opBin7) {
+        this.opBin7 = opBin7;
     }
 
-    public String getBin8() {
-        return bin8;
+    public String getOpBin8() {
+        return opBin8;
     }
 
-    public void setBin8(String bin8) {
-        this.bin8 = bin8;
+    public void setOpBin8(String opBin8) {
+        this.opBin8 = opBin8;
     }
 
-    public String getBin9() {
-        return bin9;
+    public String getOpBin9() {
+        return opBin9;
     }
 
-    public void setBin9(String bin9) {
-        this.bin9 = bin9;
-    }
-
-    public String getBoardId() {
-        return boardId;
-    }
-
-    public void setBoardId(String boardId) {
-        this.boardId = boardId;
+    public void setOpBin9(String opBin9) {
+        this.opBin9 = opBin9;
     }
 
     public String getSblMode() {
@@ -338,5 +365,197 @@ public class YieldAbnormalDto {
 
     public void setSblMode(String sblMode) {
         this.sblMode = sblMode;
+    }
+
+    public String gethInput() {
+        return hInput;
+    }
+
+    public void sethInput(String hInput) {
+        this.hInput = hInput;
+    }
+
+    public String gethYield() {
+        return hYield;
+    }
+
+    public void sethYield(String hYield) {
+        this.hYield = hYield;
+    }
+
+    public String gethPass() {
+        return hPass;
+    }
+
+    public void sethPass(String hPass) {
+        this.hPass = hPass;
+    }
+
+    public String gethBin1() {
+        return hBin1;
+    }
+
+    public void sethBin1(String hBin1) {
+        this.hBin1 = hBin1;
+    }
+
+    public String gethBin2() {
+        return hBin2;
+    }
+
+    public void sethBin2(String hBin2) {
+        this.hBin2 = hBin2;
+    }
+
+    public String gethBin3() {
+        return hBin3;
+    }
+
+    public void sethBin3(String hBin3) {
+        this.hBin3 = hBin3;
+    }
+
+    public String gethBin4() {
+        return hBin4;
+    }
+
+    public void sethBin4(String hBin4) {
+        this.hBin4 = hBin4;
+    }
+
+    public String gethBin5() {
+        return hBin5;
+    }
+
+    public void sethBin5(String hBin5) {
+        this.hBin5 = hBin5;
+    }
+
+    public String gethBin6() {
+        return hBin6;
+    }
+
+    public void sethBin6(String hBin6) {
+        this.hBin6 = hBin6;
+    }
+
+    public String gethBin7() {
+        return hBin7;
+    }
+
+    public void sethBin7(String hBin7) {
+        this.hBin7 = hBin7;
+    }
+
+    public String gethBin8() {
+        return hBin8;
+    }
+
+    public void sethBin8(String hBin8) {
+        this.hBin8 = hBin8;
+    }
+
+    public String gethBin9() {
+        return hBin9;
+    }
+
+    public void sethBin9(String hBin9) {
+        this.hBin9 = hBin9;
+    }
+
+    public String gettInput() {
+        return tInput;
+    }
+
+    public void settInput(String tInput) {
+        this.tInput = tInput;
+    }
+
+    public String gettYield() {
+        return tYield;
+    }
+
+    public void settYield(String tYield) {
+        this.tYield = tYield;
+    }
+
+    public String gettPass() {
+        return tPass;
+    }
+
+    public void settPass(String tPass) {
+        this.tPass = tPass;
+    }
+
+    public String gettBin1() {
+        return tBin1;
+    }
+
+    public void settBin1(String tBin1) {
+        this.tBin1 = tBin1;
+    }
+
+    public String gettBin2() {
+        return tBin2;
+    }
+
+    public void settBin2(String tBin2) {
+        this.tBin2 = tBin2;
+    }
+
+    public String gettBin3() {
+        return tBin3;
+    }
+
+    public void settBin3(String tBin3) {
+        this.tBin3 = tBin3;
+    }
+
+    public String gettBin4() {
+        return tBin4;
+    }
+
+    public void settBin4(String tBin4) {
+        this.tBin4 = tBin4;
+    }
+
+    public String gettBin5() {
+        return tBin5;
+    }
+
+    public void settBin5(String tBin5) {
+        this.tBin5 = tBin5;
+    }
+
+    public String gettBin6() {
+        return tBin6;
+    }
+
+    public void settBin6(String tBin6) {
+        this.tBin6 = tBin6;
+    }
+
+    public String gettBin7() {
+        return tBin7;
+    }
+
+    public void settBin7(String tBin7) {
+        this.tBin7 = tBin7;
+    }
+
+    public String gettBin8() {
+        return tBin8;
+    }
+
+    public void settBin8(String tBin8) {
+        this.tBin8 = tBin8;
+    }
+
+    public String gettBin9() {
+        return tBin9;
+    }
+
+    public void settBin9(String tBin9) {
+        this.tBin9 = tBin9;
     }
 }
