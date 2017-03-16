7439b9ef8848fa95bc7508e70f12b30944f1e305
김영호 <xfile0304@gmail.com>
2017-03-16 13:32:56 +0900

    빌드 관련 오류 파일 수정

--- betsui/src/app/90admin/dashboard/dashboard.module.ts
+++ betsui/src/app/90admin/dashboard/dashboard.module.ts
@@ -3,8 +3,8 @@ import {CommonModule} from "@angular/common";
 import {SmartadminModule} from "../../shared/smartadmin.module";
 import {DashboardComponent} from "./dashboard.component";
 import {DashboardRouting} from "./dashboard.routing";
-import "d3";
-import "nvd3";
+// import "d3";
+// import "nvd3";
 import {nvD3} from "ng2-nvd3";
 import {FlotChartModule} from "../../shared/graphs/flot-chart/flot-chart.module";
 
