234bcc927e652de63fa74e2c45e3227d2d4d761a
김영호 <xfile0304@gmail.com>
2017-03-16 14:02:33 +0900

    보드 나오는거 수정

--- src/main/java/kr/famos/bets/tdbiAnalysis/boardYield/service/BoardYieldService.java
+++ src/main/java/kr/famos/bets/tdbiAnalysis/boardYield/service/BoardYieldService.java
@@ -45,11 +45,11 @@ public class BoardYieldService {
                     mapBoardMap = new LinkedHashMap<>();
                 }
                 strBoardIdTemp = String.valueOf(board.getBoardId());
-                mapBoardMap.put("PART_NUMBER", String.valueOf(board.getBoardId()));
-                mapBoardMap.put("PROCESS_CODE", String.valueOf(board.getBoardId()));
-                mapBoardMap.put("BOARD_TYPE", String.valueOf(board.getBoardId()));
-                mapBoardMap.put("TESTER_MODEL", String.valueOf(board.getBoardId()));
-                mapBoardMap.put("TESTER_NUMBER", String.valueOf(board.getBoardId()));
+                mapBoardMap.put("PART_NUMBER", String.valueOf(board.getPartNumber()));
+                mapBoardMap.put("PROCESS_CODE", String.valueOf(board.getProcessCode()));
+                mapBoardMap.put("BOARD_TYPE", String.valueOf(board.getBoardId()).substring(0,4));
+                mapBoardMap.put("TESTER_MODEL", String.valueOf(board.getTesterModel()));
+                mapBoardMap.put("TESTER_NUMBER", String.valueOf(board.getTestNumber()));
                 mapBoardMap.put("BOARD_ID", String.valueOf(board.getBoardId()));
                 mapBoardMap.put("INPUT", "0");
                 mapBoardMap.put("PASS", "0");
