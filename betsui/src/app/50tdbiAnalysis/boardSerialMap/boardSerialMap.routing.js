1110e6017d6a896c52bd800ddb8f686518b3141b
김영호 <xfile0304@gmail.com>
2017-04-12 16:54:30 -0300

    js 파일 삭제

--- betsui/src/app/50tdbiAnalysis/boardSerialMap/boardSerialMap.routing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var router_1 = require('@angular/router');
-var boardSerialMap_component_1 = require("./boardSerialMap.component.ts");
-exports.moduleRoutes = [{
-        path: '',
-        component: boardSerialMap_component_1.YieldAbnormalComponent
-    }];
-exports.YieldAbnormalRouting = router_1.RouterModule.forChild(exports.moduleRoutes);
